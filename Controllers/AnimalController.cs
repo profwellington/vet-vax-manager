5f5b9575f0a7ea2735085b3177c668ca7446094b
Kalebe <k.gouvea@live.com>
2024-05-13 21:58:13 -0300

    Add AnimalController

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -1,11 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+using VetVaxManager.Repository;
 
 namespace VetVaxManager.Controllers
 {
     public class AnimalController : Controller
     {
+        IAnimalRepository _animalRepository;
+        public AnimalController(IAnimalRepository animalRepository)
+        {
+            _animalRepository = animalRepository;
+        }
         public IActionResult Index()
         {
+            var animals = _animalRepository.GetAnimalsByOwnerId(1);
             return View();
         }
     }
