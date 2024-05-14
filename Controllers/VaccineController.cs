a00d87e5e619e1247832c8f3f196662b8e8024cc
Kalebe Silva Gouvea <kalebe.gouvea@netconamericas.com>
2024-05-14 15:31:37 -0300

    Instancia metodo GET de VaccinationSchedules

--- Controllers/VaccineController.cs
+++ Controllers/VaccineController.cs
@@ -1,11 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using VetVaxManager.Repository;
 
 namespace VetVaxManager.Controllers
 {
     public class VaccineController : Controller
     {
+        IVaccineRepository _vaccineRepository;
+        public VaccineController(IVaccineRepository vaccineRepository)
+        {
+            _vaccineRepository = vaccineRepository;
+        }
         public IActionResult Index()
         {
+            return View();
+        }
+
+        public IActionResult VaccinationSchedule()
+        {
+            var vaccinationSchedule = _vaccineRepository.GetVaccinationSchedules();
             return View();
         }
     }
