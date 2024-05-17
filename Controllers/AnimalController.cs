d500074e23325cddc8039c0553fcbca79d5876df
Kalebe Silva Gouvea <kalebe.gouvea@netconamericas.com>
2024-05-17 15:29:48 -0300

    Add View e Action Details

--- Controllers/AnimalController.cs
+++ Controllers/AnimalController.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MySql.Data.MySqlClient;
 using System.Configuration;
+using VetVaxManager.Models;
 using VetVaxManager.Repository;
+using VetVaxManager.ViewModels;
 
 namespace VetVaxManager.Controllers
 {
     public class AnimalController : Controller
     {
         IAnimalRepository _animalRepository;
-        public AnimalController(IAnimalRepository animalRepository)
+        IVaccineRepository _vaccineRepository;
+        public AnimalController(IAnimalRepository animalRepository, IVaccineRepository vaccineRepository)
         {
             _animalRepository = animalRepository;
+            _vaccineRepository = vaccineRepository;
         }
         public IActionResult Index()
         {            
@@ -21,10 +25,15 @@ namespace VetVaxManager.Controllers
             var animals = _animalRepository.GetAnimalsByOwnerId(1);
             return View(animals);
         }
-        public IActionResult Detail(int animalId)
+        public IActionResult Details(int animalId)
         {
-            var animals = _animalRepository.GetAnimalById(1);
-            return View();
+            var animal = _animalRepository.GetAnimalById(1);
+            // TODO: Metodo para trazer informacoes de Agendas
+            var vaccines = _vaccineRepository.GetAllVaccinesByAnimalId(1);
+
+            AnimalDetailsViewModel animalDetails = null;
+
+            return View(animalDetails);
         }
     }
 }
