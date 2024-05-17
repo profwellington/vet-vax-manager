161838944949125ae791c498e6dcf5272ed86aaf
Kalebe Silva Gouvea <kalebe.gouvea@netconamericas.com>
2024-05-17 15:27:23 -0300

    Add VM para View Details

---
+++ ViewModels/AnimalDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using VetVaxManager.Models;
+
+namespace VetVaxManager.ViewModels
+{
+    public class AnimalDetailsViewModel
+    {
+        public Animal Animal { get; set; }
+        public IList<Calendar> Calendar { get; set; }
+        public IList<Vaccine> Vaccines { get; set; }
+    }
+}
